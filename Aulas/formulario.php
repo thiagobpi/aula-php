fb3aaf86bdcaad7be9169ae9a05383c257ef5577
thiago.macedo <thiago.macedo@DESKTOP-MN065HV.localdomain>
2020-08-05 10:16:16 -0300

    ts

---
+++ Aulas/formulario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>formulário de inscrição</title>
+    <meta name="autor" content = "">
+    <meta name="descrição" content="">
+    <meta name="viewpoint" content="width=device-width, initial.scale=]">
+</head>
+
+<body>
+
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES </p>
+
+<form action="script.php" method="post">
+	<p>Seu nome: <input type="text" name="nome" /></p>
+	<p>Sua idade: <input type="text" name="idade" /></p>
+	<p><input> type="submit" /></p>
+		
+</form>
+
+</body>
+
+</html>
---
+++ Aulas/script.php
@@ -0,0 +1,41 @@
+<?php
+
+//alterado no git
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+//print_r($categorias);
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+var_dump($nome);
+var_dump($idade);
+return 0;
+/*if($idade >=6 && $idade <= 12)
+{
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo "O nadador " .$nome. " compete na categoria infantil";
+    }
+}
+else if ($idade >=13 && $idade <=18)
+{
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+            echo "O nadador " .$nome. " compete na categoria adolescente";
+    }
+}
+else
+{
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+            echo "O nadador " .$nome. " compete na categoria adulto";
+    }
+}
+*/
