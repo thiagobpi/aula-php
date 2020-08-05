05018ace6fc501e148eb02132b26da5268b3c996
thiago.macedo <thiago.macedo@DESKTOP-MN065HV.localdomain>
2020-08-05 09:35:13 -0300

    commit inicial

---
+++ Aulas/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+//print_r($categorias);
+
+$nome = 'Eduardo';
+$idade = 30;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >=6 && $idade <= 12)
+{
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo "O nadador " .$nome. " compete na categoria infantil";
+    }
+}
+else if ($idade >=13 && $idade <=18)
+{ 
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+            echo "O nadador " .$nome. " compete na categoria adolescente";
+    }
+}
+else 
+{
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+            echo "O nadador " .$nome. " compete na categoria adulto";
+    }
+}
